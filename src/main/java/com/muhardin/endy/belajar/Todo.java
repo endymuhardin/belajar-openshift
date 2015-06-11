ca415a527272b668fe7da9987003677c1b50912e
Endy Muhardin <endy.muhardin@gmail.com>
2015-06-11 19:59:51 +0700

    implement todo list servlet

---
+++ src/main/java/com/muhardin/endy/belajar/Todo.java
@@ -0,0 +1,33 @@
+package com.muhardin.endy.belajar;
+
+public class Todo {
+    private Integer id;
+    private String keterangan;
+    private Boolean selesai;
+    
+    public Integer getId(){
+        return id;
+    }
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    
+    
+    public String getKeterangan(){
+        return keterangan;
+    }
+    
+    public void setKeterangan(String keterangan){
+        this.id = id;
+    }
+    
+    
+    public Boolean getSelesai(){
+        return selesai;
+    }
+    
+    public void setSelesai(Boolean selesai){
+        this.selesai = selesai;
+    }
+}
---
+++ src/main/java/com/muhardin/endy/belajar/servlet/TodoListServlet.java
@@ -0,0 +1,52 @@
+package com.muhardin.endy.belajar.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import javax.servlet.annotation.WebServlet;
+import com.muhardin.endy.belajar.Todo;
+
+
+@WebServlet("/todo/list")
+public class TodoListServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) 
+                                        throws IOException, ServletException {
+                                        
+        // Data yang akan ditampilkan, nantinya ambil dari database
+        List<Todo> daftarTodo = new ArrayList<Todo>();
+        
+        Todo t1 = new Todo();
+        t1.setId(1);
+        t1.setKeterangan("Registrasi Openshift");
+        t1.setSelesai(true);
+        daftarTodo.add(t1);
+        
+        Todo t2 = new Todo();
+        t2.setId(2);
+        t2.setKeterangan("Buat app baru");
+        t2.setSelesai(false);
+        daftarTodo.add(t2);
+        
+        Todo t3 = new Todo();
+        t3.setId(3);
+        t3.setKeterangan("Deploy Aplikasi");
+        t3.setSelesai(false);
+        daftarTodo.add(t3);
+        
+        // masukkan data ke request attribute
+        req.setAttribute("todo", daftarTodo);
+        
+        // redirect ke tampilan
+        String lokasiJsp = "/WEB-INF/jsp/todo.jsp";
+        RequestDispatcher rd = req.getRequestDispatcher(lokasiJsp);
+        rd.forward(req, res);                         
+    }
+}
